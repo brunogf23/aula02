5e0db7f2833f14e77bd46323aed6870ab086067b
Bruno <brunogfjf@gmail.com>
2024-02-21 03:47:18 -0700

    First exercise of the list.

---
+++ main.py
@@ -0,0 +1,6 @@
+
+#%% Exercise 1 sum of two integers typed by the user.
+num1 = int(input("Type the first number: "))
+num2 = int(input("Type the second number: "))
+sum12 = num1 + num2
+print(f"The sum of the two numbers is {sum12}.")
