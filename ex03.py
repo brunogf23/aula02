fdd6c5c989530558ae6f0854fea4666ebdbb5a80
Bruno <brunogfjf@gmail.com>
2024-02-21 19:54:17 -0700

    Third exercise. Multiplying numbers.

---
+++ ex03.py
@@ -0,0 +1,6 @@
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+num1 = float(input("Type the first number: "))
+num2 = float(input("Type the second number: "))
+
+result = num1*num2
+print(f"The result is: {result}")
