0625032c4c6726e2eb789723231d5197c9485992
Bruno <brunogfjf@gmail.com>
2024-02-21 19:47:38 -0700

    The second exercise.

---
+++ ex02.py
@@ -0,0 +1,4 @@
+# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+num = int(input("Please type an integer number: "))
+rem = num % 5
+print(f"The remainder of division by 5 is: {rem}")
