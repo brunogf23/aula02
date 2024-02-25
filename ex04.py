4915378172df5316862a1197220fad30b4acb5c3
Bruno <brunogfjf@gmail.com>
2024-02-25 07:01:43 -0700

    Forth exercise. Dividing numbers.

---
+++ ex04.py
@@ -0,0 +1,5 @@
+# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+num1 = int(input("Type the first integer number: "))
+num2 = int(input("Type the second integer number: "))
+sum12 = num1 // num2
+print(f"The division of the two numbers is {sum12}.")
